7d164a22facb254e6f4a66559ba838a9e2bfbea8
Artemiy Moroz <artemiy.moroz@gmail.com>
2024-11-04 22:18:52 +0200

    Added generated Javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Canvas"},{"p":"<Unnamed>","l":"Pic"},{"p":"<Unnamed>","l":"PicFactory"},{"p":"<Unnamed>","l":"PicType"}];updateSearchResults();
